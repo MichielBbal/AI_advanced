efdc0a563916d992a9dc46df9734ebea739098de
Michiel Bontenbal <37507786+MichielBbal@users.noreply.github.com>
2023-08-26 18:51:49 +0200

    25/8

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-openai_key = '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'
+openai_key = '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'
 wolframalpha_key = '6HYTWL-Q4R5YU5JG9'
+elevenlabs_key ='242369c63b0aaef3e203bd09bc05ec56'
+cohere_key = 'kWWNHxy1gyMljLopnNMMXxLP0sN3fO7u94lJV2xd'
+weaviate_key = '76320a90-53d8-42bc-b41d-678647c6672e' #demo database
